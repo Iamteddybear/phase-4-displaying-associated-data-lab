57e7fd95b4e07291bd33549ed491118fe6433298
ned <nedschultz@outlook.com>
2023-07-05 14:34:28 +1000

    finished

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all
+  
+      render json: items.map { |item|
+        {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price,
+          user: {
+            id: item.user.id,
+            username: item.user.username,
+            city: item.user.city
+          }
+        }
+      }
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    items = user.items
+
+    render json: {
+      id: user.id,
+      username: user.username,
+      city: user.city,
+      items: items.map { |item|
+        {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price
+        }
+      }
+    }
+  end
 end
